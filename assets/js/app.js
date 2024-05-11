4224ba7ac59e2b5a97b45311b631ee812c5770c5
unknown <abasian.arman@gmail.com>
2024-05-11 06:26:11 +0330

    first commit

---
+++ assets/js/app.js
@@ -0,0 +1,143 @@
+if ("serviceWorker" in navigator) {
+  navigator.serviceWorker
+    .register("/sw.js")
+    .then(function (res) {})
+    .catch(function (e) {
+      console.log(e.message);
+    });
+}
+
+// Check browser------------------
+navigator.sayswho = () => {
+  var N = Navigator.appName,
+    ua = navigator.userAgent,
+    tem,
+    M = ua.match(/(opera|chrome|safari|firefox|msie)\/?\s*([\d\.]+)/i);
+  if (M && (tem = ua.match(/version\/([\.\d]+)/i)) != null) M[2] = tem[1];
+  M = M ? [M[1], M[2]] : [N, Navigator.appVersion, "-?"];
+  return M.join(" ");
+};
+//if (!navigator.sayswho().includes("Chrome")) alert("لطفا از مرورگر کروم استفاده کنید")
+
+// install prompt-------------
+var deferredPrompt;
+window.addEventListener("beforeinstallprompt", (e) => {
+  if (!window.matchMedia("(display-mode: standalone)").matches) {
+    setTimeout(() => {
+      document.getElementById("install-prompt")?.classList.add("show");
+    }, 5000);
+  }
+  e.preventDefault();
+  deferredPrompt = e;
+  return false;
+});
+
+// install prompt in ios------------------
+const isIos = () => {
+  const userAgent = window.navigator.userAgent.toLowerCase();
+  return /iphone|ipad|ipod/.test(userAgent);
+};
+
+const isInStandaloneMode = () =>
+  "standalone" in window.navigator && window.navigator.standalone;
+
+// Checks if should display install popup notification:
+if (isIos() && !isInStandaloneMode()) {
+  // this.setState({ showInstallMessage: true });
+  this.popupIos = true;
+}
+
+document.getElementById("install-prompt")?.addEventListener("click", (e) => {
+  if (deferredPrompt) {
+    deferredPrompt.prompt();
+    e.target.classList.remove("show");
+    deferredPrompt.userChoice.then((choiceRes) => {
+      console.log(choiceRes.outcome);
+      if (choiceRes.outcome === "accepted") {
+        console.log("User accepted the install prompt.");
+      } else if (choiceRes.outcome === "dismissed") {
+        console.log("User dismissed the install prompt");
+      }
+    });
+    deferredPrompt = null;
+  }
+});
+
+document.addEventListener("DOMContentLoaded", function () {
+  var elems = document.querySelectorAll(".sidenav");
+  var instances = M.Sidenav.init(elems, {
+    edge: "right",
+  });
+});
+
+// Push notification-------------------
+
+//if the user do not certain the condition about allowance of push notification
+if (Notification.permission == "default") {
+  setTimeout(() => {
+    document
+      .getElementById("notification_bell_box")
+      ?.classList.remove("dis-none");
+  }, 10000);
+}
+
+const showConfirmNotify = () => {
+  if ("serviceWorker" in navigator) {
+    navigator.serviceWorker.ready.then((sw) => {
+      sw.showNotification("ممنون از شما...!");
+      sw.pushManager
+        .subscribe({
+          userVisibleOnly: true,
+          applicationServerKey:
+            "BNbqX8M5NJJfs_IcL_5Gfisx7FkOYHtYniD4QMJq1RB4DeQsOmGo3lO-zzurFEqTUwtrqQHKb62p_TzxPU552yI",
+        })
+        .then((subscription) => {
+          fetch("https://pushnotif.azhadev.ir/api/push-subscribe", {
+            method: "post",
+            body: JSON.stringify(subscription),
+          })
+            .then((res) => {
+              return res.json();
+            })
+            .then((response) => {
+              console.log(response);
+              alert("این کد رو ذخیره کنید : " + response.user_code);
+            });
+        });
+    });
+  }
+};
+
+//this handler is related to click on bell icon for accept or reject-
+//nofitication allowance
+document
+  .getElementById("notification_bell_box")
+  ?.addEventListener("click", () => {
+    //this method get the option that user choosed in browser nofit pop-up
+    Notification.requestPermission((res) => {
+      if (res == "granted") {
+        showConfirmNotify();
+      } else {
+        console.log("Blocked...!");
+      }
+    });
+  });
+
+// network status------------------------
+
+const setnetworkStatus = (status) => {
+  const iconElem = document.querySelector("#network_status i");
+  if (status == 1) {
+    iconElem?.classList.add("green-text");
+    iconElem?.classList.remove("red-text");
+  } else {
+    iconElem?.classList.remove("green-text");
+    iconElem?.classList.add("red-text");
+  }
+};
+
+if (navigator.onLine) setnetworkStatus(1);
+else setnetworkStatus(0);
+
+window.addEventListener("online", () => setnetworkStatus(1));
+window.addEventListener("offline", () => setnetworkStatus(0));
---
+++ sw.js
@@ -0,0 +1,186 @@
+importScripts("/assets/js/idb.js");
+importScripts("/assets/js/idbUtils.js");
+
+//list of static caches(we derived that from dependencies in index.html file)
+let staticItems = [
+  "/",
+  "/index.html",
+  "/offline.html",
+  "/assets/materialize/css/materialize.min.css",
+  "/assets/css/util.css",
+  "https://fonts.googleapis.com/icon?family=Material+Icons",
+  "/assets/css/style.css",
+  "/assets/js/idb.js",
+  "/assets/js/idbUtils.js",
+  "/assets/js/alpineJsContollers/usersController.js",
+  "/assets/materialize/js/materialize.min.js",
+  "/assets/js/app.js",
+  "https://fonts.gstatic.com/s/materialicons/v140/flUhRq6tzZclQEJ-Vdg-IuiaDsNcIhQ8tQ.woff2",
+];
+
+//we use from variable for names of caches to change them easier in the future
+//also for changing the version
+let STATIC_CACHE = "static-v2";
+let DYNAMIC_CACHE = "dynamic";
+
+//this method is for removing exsesive chache(we write it ourself)
+//for example: we said we must have 5 item in chache
+
+const trimCache = (chachName, max) => {
+  caches.open(chachName).then((cache) => {
+    //keys=> list of cache names as array=>[Request, Request, Request, Request, Request]
+    return cache.keys().then((keys) => {
+      //if the length of caches was more than limitaion
+      if (keys.length > max) {
+        //delete the first one in array
+        cache.delete(keys[0]).then(() => {
+          //and again run the method(thid method continue until num of cahches reach to limitaion)
+          trimCache(chachName, max);
+        });
+      }
+    });
+  });
+};
+
+// install event -------------------------------------->>
+//we add to static caches in install level
+self.addEventListener("install", function (e) {
+  e.waitUntil(
+    caches.open(STATIC_CACHE).then((cache) => {
+      cache.addAll(staticItems);
+    })
+  );
+});
+
+// activate event ------------------------------------>>
+self.addEventListener("activate", function (e) {
+  e.waitUntil(
+    //this method is for managing the versions of caches and delete the old caches
+    caches.keys().then((keys) => {
+      console.log(keys);
+      return Promise.all(
+        keys.map((key) => {
+          if (key != STATIC_CACHE && key != DYNAMIC_CACHE) {
+            console.log("Deleting Cache:", key);
+            return caches.delete(key);
+          }
+        })
+      );
+    })
+  );
+  return self.clients.claim();
+});
+
+// fetch event -------------------------------------->>
+self.addEventListener("fetch", function (e) {
+  //if the url of the caches was a ajax type
+  //use from fetch only strategy
+  if (
+    e.request.url.indexOf("https://jsonplaceholder.typicode.com/users") > -1
+  ) {
+    e.respondWith(
+      caches.open(DYNAMIC_CACHE).then((cache) => {
+        return fetch(e.request).then((response) => {
+          trimCache(DYNAMIC_CACHE, 10);
+          cache.put(e.request, response.clone());
+          return response;
+        });
+      })
+    );
+    //else use from chache first strategy
+  } else {
+    e.respondWith(
+      //if the fetched item was inadvanced in cache list
+      caches.match(e.request).then((res) => {
+        return (
+          res ||
+          fetch(e.request)
+            .then((fetchRes) => {
+              return caches.open(DYNAMIC_CACHE).then((cache) => {
+                trimCache(DYNAMIC_CACHE, 10);
+                cache.put(e.request, fetchRes.clone());
+                return fetchRes;
+              });
+            })
+            //if we did not have that item in our cache and also we have not internet to fetch it
+            //if the kinds of item is 'text/html' => show the offline.html file to user
+            .catch((err) => {
+              return caches.open(STATIC_CACHE).then((cache) => {
+                if (e.request.headers.get("accept").includes("text/html")) {
+                  return cache.match("/offline.html");
+                }
+              });
+            })
+        );
+      })
+    );
+  }
+});
+
+// notification -------------------------------
+
+//this event is when we show a notif to user and user click on-
+//the options in notification
+self.addEventListener("notificationclick", (event) => {
+  if (event.action == "confirm") {
+    console.log("اکشن مورد نظر تایید شد...!");
+  } else if (event.action == "cancel") {
+    event.notification.close();
+    console.log("اکشن مورد نظر نادیده گرفته شد...!");
+  } else {
+    event.waitUntil(clients.openWindow(event.notification.data.notifUrl));
+    console.log("اکشنی انتخاب نشد...!");
+  }
+});
+
+//when user click on * button to close the notification
+self.addEventListener("notificationclose", (event) => {
+  console.log("notification closed...!");
+});
+
+//this event is related to push a new notification
+self.addEventListener("push", (event) => {
+  const notification = event.data.json();
+  const options = {
+    body: notification.body,
+    icon: "/assets/images/codeyadIcon.png",
+    image: "/assets/images/office.jpg",
+    dir: "ltr",
+    vibrate: [100, 50, 200],
+    badge: "/assets/images/codeyadIcon.png",
+    tag: "group1",
+    renotify: true,
+    actions: [
+      { action: "confirm", title: "تایید", icon: "/assets/images/confirm.png" },
+      { action: "cancel", title: "انصراف", icon: "/assets/images/cancel.png" },
+    ],
+    data: {
+      notifUrl: notification.url,
+    },
+  };
+  self.registration.showNotification(notification.title, options);
+});
+
+// background sync -------------------
+const thisSw = self;
+self.addEventListener("sync", (event) => {
+  console.log("Background sync event started...", event);
+  if (event.tag == "syncPostData") {
+    console.log("Sync post data started...");
+    readAllData("postDataStore").then((data) => {
+      for (const d of data) {
+        fetch("https://jsonplaceholder.typicode.com/users", {
+          method: "POST",
+          body: JSON.stringify(d),
+        })
+          .then((res) => res.json())
+          .then(() => {
+            deleteOneData("postDataStore", d.id).then(() => {
+              console.log(d.id, "deleted...");
+              thisSw.registration.showNotification("ارسال اطلاعات انجام شد");
+            });
+          });
+      }
+    });
+  }
+});
